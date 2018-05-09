6a3048edba60b866060a06c8e372d8487479dac7
Charisse <charisse.ysabel930@gmail.com>
2018-05-09 20:48:46 +0200

    feat(Table) Wire up basic edit item functionality copied from the Form component

--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -1,15 +1,20 @@
 import React, { Component } from 'react';
+import { map } from 'lodash/fp';
 import axios from 'axios';
 
+const CATEGORY_OPTIONS = ['none', 'grocery', 'auto', 'clothing', 'transportation'];
+const ACCOUNT_OPTIONS = ['none', 'cash', 'checkingAccount', 'credit'];
 
 export default class Table extends Component {
     constructor(props) {
         super(props);
         this.state = {
             data: [],
+            itemToEdit: {},
         };
 
         this.handleRemoveItem = this.handleRemoveItem.bind(this);
+        this.handleEditItem = this.handleEditItem.bind(this);
     }
 
     componentDidMount() {
@@ -35,6 +40,12 @@ export default class Table extends Component {
         e.preventDefault();
     }
 
+    handleEditItem(e, item) {
+        this.setState({
+            itemToEdit: { ...item },
+        });
+    }
+
     render() {
         if (this.state.data.length === 0) {
             return <div>Loading...</div>;
@@ -53,13 +64,69 @@ export default class Table extends Component {
                                     <td>{item['amount']}</td>
                                     <td>
                                         <button onClick={e => this.handleRemoveItem(e, item)}>Remove</button>
+                                        <button onClick={e => this.handleEditItem(e, item)}>Edit</button>
                                     </td>
                                 </tr>
                             );
                         })}
                     </tbody>
                 </table>
+
+                {this.state.itemToEdit.name != undefined ? (
+                    <div>
+                        <form>
+                            <div>
+                                <label htmlFor="transactionName">Name</label>
+                                <input
+                                    type="text"
+                                    name="transactionName"
+                                    id="transactionName"
+                                    value={this.state.itemToEdit.name}
+                                />
+                            </div>
+
+                            <div>
+                                <label htmlFor="category">Category</label>
+                                <Select values={CATEGORY_OPTIONS} selected={this.state.itemToEdit.category} />
+                            </div>
+
+                            <div>
+                                <label htmlFor="account">Account</label>
+                                <Select values={ACCOUNT_OPTIONS} selected={this.state.itemToEdit.account} />
+                            </div>
+
+                            <div>
+                                <label htmlFor="transactionAmount">Amount</label>
+                                <input
+                                    type="text"
+                                    name="transactionAmount"
+                                    id="transactionAmount"
+                                    value={this.state.itemToEdit.amount}
+                                />
+                            </div>
+
+                            <button type="submit" onSubmit={e => this.handleOnSubmit(e)}>
+                                Submit
+                            </button>
+                        </form>
+                    </div>
+                ) : null}
             </div>
         );
     }
 }
+
+const Select = ({ values, selected }) => {
+    return (
+        <select value={selected}>
+            {map(
+                value => (
+                    <option value={value} key={value}>
+                        {value}
+                    </option>
+                ),
+                values
+            )}
+        </select>
+    );
+};
