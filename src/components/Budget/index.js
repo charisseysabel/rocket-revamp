d90d76d2a7d9aeac5e00309e5e5d23de2cabc00b
Charisse <charisse.ysabel930@gmail.com>
2018-06-24 18:56:35 +0200

    feat(Budget) Wire logic to API

--- src/components/Budget/index.js
+++ src/components/Budget/index.js
@@ -1,4 +1,6 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, Component } from 'react';
+import axios from 'axios';
+import { BUDGET } from '../../api';
 import Budget from './presenter';
 
 const CREATE_BUDGET_DIALOG = 'create-budget-dialog';
@@ -26,25 +28,73 @@ export default function Container() {
     );
 }
 
-function Dialog({ handleClose }) {
-    return (
-        <dialog id={CREATE_BUDGET_DIALOG}>
-            <form>
-                <fieldset>
-                    <label htmlFor="budget">Budget Name</label>
-                    <input type="text" name="budget" id="budget" />
+class Dialog extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            budget: '',
+            amount: '',
+        };
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+    }
 
-                    <label htmlFor="amount">Amount</label>
-                    <input type="text" name="amount" id="amount" />
-                </fieldset>
-                <div>
-                    <button>Create Another</button>
-                </div>
-                <div>
-                    <button>Create</button>
-                </div>
-            </form>
-            <button onClick={e => handleClose(e)}>Cancel</button>
-        </dialog>
-    );
+    handleSubmit(e, item) {
+        axios
+            .post(BUDGET, {
+                ...item,
+            })
+            .then(res => {
+                console.log(res);
+            })
+            .catch(function(err) {
+                console.log(err);
+            });
+        e.preventDefault();
+    }
+
+    handleChange(e, type) {
+        console.log('changing!');
+        this.setState({
+            [type]: e.target.value,
+        });
+        e.preventDefault();
+    }
+
+    render() {
+        const { budget, amount } = this.state;
+        const { handleClose } = this.props;
+        return (
+            <dialog id={CREATE_BUDGET_DIALOG}>
+                <form onSubmit={e => this.handleSubmit(e, { ...this.state })}>
+                    <fieldset>
+                        <label htmlFor="budget">Budget Name</label>
+                        <input
+                            type="text"
+                            name="budget"
+                            id="budget"
+                            value={budget}
+                            onChange={e => this.handleChange(e, 'budget')}
+                        />
+
+                        <label htmlFor="amount">Amount</label>
+                        <input
+                            type="text"
+                            name="amount"
+                            id="amount"
+                            value={amount}
+                            onChange={e => this.handleChange(e, 'amount')}
+                        />
+                    </fieldset>
+                    <div>
+                        <button>Create Another</button>
+                    </div>
+                    <div>
+                        <button type="submit">Create</button>
+                        <button onClick={e => handleClose(e)}>Cancel</button>
+                    </div>
+                </form>
+            </dialog>
+        );
+    }
 }
